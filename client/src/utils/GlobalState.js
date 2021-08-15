27dd075148dcd42ef57eb5c99bec2b923b003a91
JAPinell <japinell@yahoo.com>
2021-08-15 01:30:01 -0500

    Converted GlobalState to react-reduct provider

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -8,3 +8,24 @@ function StoreProvider(props) {
 }
 
 export { StoreProvider };
+
+//const StoreContext = createContext();
+//const { Provider } = StoreContext;
+
+// const StoreProvider = ({ value = [], ...props }) => {
+//   const [state, dispatch] = useProductReducer({
+//     products: [],
+//     cart: [],
+//     cartOpen: false,
+//     categories: [],
+//     currentCategory: "",
+//   });
+
+//   return <Provider value={[state, dispatch]} {...props} />;
+// };
+
+// const useStoreContext = () => {
+//   return useContext(StoreContext);
+// };
+
+// export { StoreProvider, useStoreContext };
